776e400353050b3506ec3db4ab00f0fbf91f3987
Gregory R. Lee <grlee77@gmail.com>
2018-01-24 14:15:27 -0500

    fix plotting in wp_scalogram.py demo

--- demo/wp_scalogram.py
+++ demo/wp_scalogram.py
@@ -12,7 +12,7 @@ data = np.sin(250 * np.pi * x**2)
 
 wavelet = 'db2'
 level = 4
-order = "freq"  # other option is "normal"
+order = "freq"  # other option is "natural"
 interpolation = 'nearest'
 cmap = plt.cm.cool
 
@@ -24,29 +24,27 @@ values = np.array([n.data for n in nodes], 'd')
 values = abs(values)
 
 # Show signal and wavelet packet coefficients
-fig = plt.figure()
-fig.subplots_adjust(hspace=0.2, bottom=.03, left=.07, right=.97, top=.92)
-ax = fig.add_subplot(2, 1, 1)
-ax.set_title("linchirp signal")
-ax.plot(x, data, 'b')
-ax.set_xlim(0, x[-1])
+fig, (ax1, ax2) = plt.subplots(2, 1)
+fig.subplots_adjust(hspace=0.4, bottom=.06, left=.09, right=.96, top=.92)
+ax1.set_title("linchirp signal")
+ax1.plot(x, data, 'b')
+ax1.set_xlim(0, x[-1])
 
-ax = fig.add_subplot(2, 1, 2)
-ax.set_title("Wavelet packet coefficients at level %d" % level)
-ax.imshow(values, interpolation=interpolation, cmap=cmap, aspect="auto",
-          origin="lower", extent=[0, 1, 0, len(values)])
-ax.set_yticks(np.arange(0.5, len(labels) + 0.5), labels)
+ax2.set_title("Wavelet packet coefficients at level %d" % level)
+ax2.imshow(values, interpolation=interpolation, cmap=cmap, aspect="auto",
+           origin="lower", extent=[0, 1, 0, len(values)])
+ax2.set_yticks(np.arange(0.5, len(labels) + 0.5))
+ax2.set_yticklabels(labels)
 
 # Show spectrogram and wavelet packet coefficients
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(211)
-ax2.specgram(data, NFFT=64, noverlap=32, Fs=2, cmap=cmap,
+fig, (ax3, ax4) = plt.subplots(2, 1)
+fig.subplots_adjust(hspace=0.4)
+ax3.specgram(data, NFFT=64, noverlap=32, Fs=2, cmap=cmap,
              interpolation='bilinear')
-ax2.set_title("Spectrogram of signal")
-ax3 = fig2.add_subplot(212)
-ax3.imshow(values, origin='upper', extent=[-1, 1, -1, 1],
+ax3.set_title("Spectrogram of signal")
+ax4.imshow(values, origin='upper', extent=[-1, 1, -1, 1],
            interpolation='nearest')
-ax3.set_title("Wavelet packet coefficients")
+ax4.set_title("Wavelet packet coefficients")
 
 
 plt.show()
